a4d01919ba137590e059c7fe2c1f78435bd38df9
Federico Bett <gfbett@gmail.com>
2020-08-14 12:41:25 -0300

    Created train and test datasets

--- utils/dataset_generator.py
+++ utils/dataset_generator.py
@@ -1,6 +1,22 @@
 import h5py
 import cv2
 import numpy as np
+import os
+import random
+
+
+def get_images_list():
+    list = []
+    add_images("../images/", list)
+    return list
+
+
+def add_images(dir, list):
+    for entry in os.scandir(dir):
+        if entry.is_file():
+            list.append(entry.path)
+        elif entry.is_dir():
+            add_images(entry.path, list)
 
 
 def read_image(path):
@@ -9,31 +25,42 @@ def read_image(path):
     return image
 
 
-def create_dataset(filelist):
-    f = h5py.File("../dataset/train.h5", "w")
+def create_dataset(filelist, name):
+    f = h5py.File(f"../dataset/{name}.h5", "w")
+    # Create train dataset
     n = len(filelist)
-    f.create_dataset("train_images", (n, 32*32), np.uint8)
-    f.create_dataset("train_tags", (n, 1), np.uint8)
+    f.create_dataset(f"{name}_images", (n, 32*32), np.uint8)
+    f.create_dataset(f"{name}_tags", (n, 1), np.uint8)
 
+    print(f"Adding {n} images to {name} dataset")
     for i in range(n):
         path = filelist[i]
         img = read_image(path)
-        f["train_images"][i] = img
-        f["train_tags"][i] = 1 if "/a/" in path else 0
+        f[f"{name}_images"][i] = img
+        f[f"{name}_tags"][i] = 1 if "/a/" in path else 0
 
     f.close()
 
+def create_datasets(training_set, test_set):
+    create_dataset(training_set, "train")
+    create_dataset(test_set, "test")
 
 
+def split_sets(images, split):
+    n = len(images)
+    training_size = int(n * split / 100)
+    training_set = []
+    for i in range(training_size):
+        image = random.choice(images)
+        training_set.append(image)
+        images.remove(image)
+
+    return training_set, images
 
 
 if __name__ == '__main__':
-    filelist = [
-        "../images/a/img0.png",
-        "../images/a/img1.png",
-        "../images/a/img2.png",
-        "../images/b/img0.png",
-        "../images/b/img1.png",
-        "../images/b/img2.png"
-    ]
-    create_dataset(filelist)
+    images = get_images_list()
+    training_set, test_set = split_sets(images, 80)
+    # print(len(training_set))
+    # print(len(test_set))
+    create_datasets(training_set, test_set)
--- utils/test.py
+++ utils/test.py
@@ -2,9 +2,11 @@ import h5py
 import cv2
 import numpy as np
 
-f = h5py.File("../dataset/train.h5", "r")
-print(f["train_images"].shape)
-print(f["train_tags"].shape)
-print(np.array(f["train_tags"]))
+with h5py.File("../dataset/train.h5", "r") as f:
+    print("train_images: ", f["train_images"].shape)
+    print("train_tags: ", f["train_tags"].shape)
 
-f.close()
+with h5py.File("../dataset/test.h5", "r") as f:
+    print("test_images: ", f["test_images"].shape)
+    print("test_tags: ", f["test_tags"].shape)
+
